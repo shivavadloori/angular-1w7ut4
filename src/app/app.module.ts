5d0e59d6f0edea9e388325d6d02de68014c137f9
shivavadloori <vadloorishiva@gmail.com>
2020-05-11 19:43:32 +0530

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { routing }        from './app.routing';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HiddenDirective} from './customdirectives/hidden.directive';
 import {UnderLineDirective} from './customdirectives/underline.directive';
+import {LoginComponent} from './login/login.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ,HiddenDirective,UnderLineDirective],
+  imports:      [ BrowserModule, FormsModule,routing],
+  declarations: [ AppComponent, HelloComponent ,HiddenDirective,UnderLineDirective,LoginComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
